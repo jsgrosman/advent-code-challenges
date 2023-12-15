3b495e05c4ee055a60783631170c7055d6d6a671
Jason Grosman <jgrosman@straighterline.com>
2023-12-15 01:23:46 -0500

    Advent 2023 Day 15

---
+++ advent2023/advent15.ts
@@ -0,0 +1,46 @@
+    import { getFileContents } from "../Utils";
+    
+    
+    
+    const part1and2 = () => {
+        const contents = getFileContents();
+        const codes = contents.trim().split(',');
+
+        const hash = (value: string) => {
+            return value.split('').reduce( (p, v) => {return ((p + v.charCodeAt(0)) * 17) % 256;}, 0);     
+        }
+
+        const hashTotal = codes.reduce( (p, v) => p + hash(v), 0);
+        console.log(`Part 1 Total: ${hashTotal}`);
+
+        const lensMap = new Map<number, string[]>(Array.from({ length: 256 }, (_, i) => [i, []]));
+        for (let code of codes) {
+            if (code.includes('-')) {
+                const [label] = code.split('-');
+                const boxNumber = hash(label);
+                lensMap.set(boxNumber, lensMap.get(boxNumber)!.filter( (v) => !v.startsWith(label)));
+            } else if (code.includes('=')) {
+                const [label, focalLength] = code.split('=');
+                const boxNumber = hash(label);
+                const boxList = lensMap.get(boxNumber)!;
+                
+                const foundIndex = boxList.findIndex(box => box.startsWith(label));
+                foundIndex !== -1
+                ? (boxList[foundIndex] = `${label} ${focalLength}`)
+                : boxList.push(`${label} ${focalLength}`);
+            }
+        }
+        
+        let lensTotal = 0;
+        for (const [boxNumber, boxList] of lensMap.entries()) {
+            if (boxList.length > 0) {
+                lensTotal += boxList.reduce( (p, v, index) => {
+                    return p + ((boxNumber + 1) * (index + 1) * Number(v.split(' ')[1]));
+                }, 0);
+            }
+        }
+        console.log(`Total Part 2: ${lensTotal}`);
+    };
+    
+
+    part1and2();
